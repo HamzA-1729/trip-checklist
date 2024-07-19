9b3ba7c3f908e4e4675275d793e054b56d1ee16b
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-07-19 17:05:59 +0500

    A controlled form element added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!description) return;
+    setDescription("");
+    setQuantity(1);
+  };
+
   return (
-    <div className="add-from">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip? </h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+        placeholder="Item..."
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
