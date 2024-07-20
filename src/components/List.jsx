0708f34f6d987abdf7548b46254090417e0df975
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-07-20 15:57:19 +0500

    New items displayed correctly added through form component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,18 @@
-function List() {
-  return <div className="list">LIST</div>;
+import Item from "./Item";
+function List({ items, onDeleteItems, onToggleItem }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItems={onDeleteItems}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 export default List;
