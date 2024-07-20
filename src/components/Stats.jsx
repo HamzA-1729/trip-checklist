8674c65bd3cfc26575a9c773e9a6672ac88c20a5
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-07-20 15:58:09 +0500

    Conditional rendering added

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,21 @@
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packaging list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const packedPercentage = Math.round((packedItems / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {packedPercentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 You have${numItems} items on your list, and you already packed
+        ${packedItems} (${packedPercentage}%)`}
+      </em>
     </footer>
   );
 }
