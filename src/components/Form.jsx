1784d1e4e526d751e734f12075b1034842872054
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-07-20 15:53:36 +0500

    Form full fucntionality added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,22 @@
 import { useState } from "react";
-
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip? </h3>
+      <h3>What items do you need for your 😍 trip? </h3>
       <select
         value={quantity}
-        onChange={(e) => {
-          setQuantity(Number(e.target.value));
-        }}
+        onChange={(e) => setQuantity(Number(e.target.value))}
       >
         {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
           <option value={num} key={num}>
@@ -28,9 +27,7 @@ function Form() {
       <input
         type="text"
         value={description}
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
+        onChange={(e) => setDescription(e.target.value)}
         placeholder="Item..."
       />
       <button>Add</button>
