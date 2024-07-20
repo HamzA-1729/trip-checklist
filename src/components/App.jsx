dd14d788a9acd651928533189f770263b714655d
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-07-20 16:00:41 +0500

    State has been lifted and necessary functionality added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Form from "./Form";
 import List from "./List";
 import Logo from "./Logo";
 import Stats from "./Stats";
 import "./index.css";
 function App() {
+  const [items, setItems] = useState([]);
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+  const handleDeleteItems = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+  const handleToggleItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <List />
-      <Stats />
+      <Form onAddItems={handleAddItems} />
+      <List
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
